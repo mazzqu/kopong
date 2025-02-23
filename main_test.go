829d927a434ad66f77402f1b03f6cc4bc1265ee9
user mac <usermac@users-MacBook-Pro.local>
2025-02-23 13:54:23 +0800

    after modify test

--- main_test.go
+++ main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestGreeting(t *testing.T) {
-	name := "alize"
-	want := "hello alize"
+	name := "lord jengkloz"
+	want := "hello lord jengkloz"
 
 	if got := greeting(name); got != want {
 		t.Errorf("greeting(%q) = %q, want %q", name, got, want)
